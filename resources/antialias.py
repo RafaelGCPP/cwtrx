de7faa0f9ee28b485f7838e6da03efba535b4b23
Rafael Pinto <rafaelgcpp@gmail.com>
2023-12-11 13:30:47 -0300

    implementing SK filters

--- resources/antialias.py
+++ resources/antialias.py
@@ -1,12 +1,12 @@
 from antialias_proto import antialias_proto
 
-if __name__=='__main__':
+#if __name__=='__main__':
 
-  BW=4000
-  Fs=10000
-  nbits=12
-  Rp=0.3
-  (N,wn,sos_proto)=antialias_proto(BW,Fs,nbits,Rp)
+BW=10000
+Fs=100000
+nbits=12
+Rp=0.3
+(N,wn,sos_proto)=antialias_proto(BW,Fs,nbits,Rp)
 
-  print("{0}-th order Chebyshev Type I".format(N))
-  print(sos_proto)
+print("{0}-th order Chebyshev Type I".format(N))
+print(sos_proto)
--- resources/sallen_key.py
+++ resources/sallen_key.py
@@ -1,12 +1,36 @@
-class SallenKeyLPCascade:
+
+
+class SallenKeyCascade:
     def __init__(self,sos,wn):
+        self.cascade=[]
+        for f in sos:
+            if f[3]==0:
+                self.cascade.append(SallenKeySection(f[0:2],f[3:5],wn))
 
-class FirstOrderLP:
+class SallenKeySection:
     def __init__(self,num,den,wn):
-        pass
+        self.num=num
+        self.den=den
+        self.wn=wn
 
-class SKSecondOrderLP:
-    def __init__(self,num,den,wn):
-        pass
+        #TODO check for zeros in numerator and choose filter type
+
+        if num[0]==0:  # either a1 s+ a0 or a0
+            if num[1]==0: # a0
+                pass
+            else:
+                pass
+        elif num[1]==0: # either a2 s^2 + a0 or a2 s^2
+            if num[2]==0: # a2 s^2
+                pass
+            else:
+                pass
+        else: # general case a2 s^2 + a1 s + a0
+            pass
+            
+        
+        #TODO check filter order (denominator)   
+
+        
 
 
