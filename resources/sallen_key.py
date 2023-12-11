76b115a63417438155953fbb1b9dc49f5085dce5
Rafael Pinto <rafael.gustavo@petrobras.com.br>
2023-12-11 15:49:35 -0300

    pydocs

--- resources/sallen_key.py
+++ resources/sallen_key.py
@@ -1,6 +1,10 @@
 
 
 class SallenKeyCascade:
+    """
+    Given the SOS (second-order section) output of an analog filter prototype design
+    by scipy.signal, creates a cascade of Sallen and Key biquads.
+    """
     def __init__(self,sos,wn):
         self.cascade=[]
         for f in sos:
@@ -8,6 +12,11 @@ class SallenKeyCascade:
                 self.cascade.append(SallenKeySection(f[0:2],f[3:5],wn))
 
 class SallenKeySection:
+    """
+    Parent class of the many different SK filters.
+    On the constructor, it chooses the best configuration based
+    on the filter type, poles and zeros.
+    """
     def __init__(self,num,den,wn):
         self.num=num
         self.den=den
@@ -25,12 +34,14 @@ class SallenKeySection:
         if num[0]==0:  # either a1 s+ a0 or a0
             if num[1]==0: # a0
                 #TODO implement all pole low pass
+                # EPF LP 
                 pass
                 return
             else:
                 pass
         elif num[1]==0: # either a2 s^2 + a0 or a2 s^2
             if num[2]==0: # a2 s^2
+                #TODO implement high pass (zeros at the origin)
                 pass
             else:
                 pass
