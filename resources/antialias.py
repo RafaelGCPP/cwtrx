385bd1d7e5aecd880f21276a0000ee7ab73671c1
Rafael Pinto <rafael.gustavo@petrobras.com.br>
2023-12-11 09:45:07 -0300

    refactoring

--- resources/antialias.py
+++ resources/antialias.py
@@ -1,17 +1,12 @@
-from scipy.signal import cheby1, cheb1ord
-from math import pi, log10
+from antialias_proto import antialias_proto
 
-BW=4000
-Fs=10*BW
-nbits=12
-Rp=0.3
+if __name__=='__main__':
 
-Wp=2*pi*BW
-Ws=2*pi*(Fs-BW)
-Rs=nbits*20*log10(2)
+  BW=4000
+  Fs=10*BW
+  nbits=12
+  Rp=0.3
+  (N,sos_proto)=antialias_proto(BW,Fs,nbits,Rp)
 
-(N,wn)=cheb1ord(Wp, Ws, Rp, Rs, analog=True)
-sos_proto=cheby1(N,Rp,1,analog=True,output='sos')
-
-print("{0}-th order Chebyshev Type I".format(N))
-print(sos_proto)
+  print("{0}-th order Chebyshev Type I".format(N))
+  print(sos_proto)
---
+++ resources/antialias_proto.py
@@ -0,0 +1,14 @@
+from scipy.signal import cheby1, cheb1ord
+from math import pi, log10
+
+
+def antialias_proto(bw,fs,nbits,rp):
+
+  Wp=2*pi*bw
+  Ws=2*pi*(fs-bw)
+  Rs=nbits*20*log10(2)
+
+  (N,wn)=cheb1ord(Wp, Ws, rp, Rs, analog=True)
+  sos_proto=cheby1(N,rp,1,analog=True,output='sos')
+  return (N,sos_proto)
+
